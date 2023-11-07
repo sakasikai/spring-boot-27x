7ee3c91ac4b51e628c386e85f6c2f3e30196d56b
zhangxiaoqi <tonq_dlut@126.com>
2023-11-07 22:37:24 +0800

    pre:compile source code

---
+++ tonqlet-room/src/main/java/org/springframework/boot/Main.java
@@ -0,0 +1,16 @@
+package org.springframework.boot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @title ${NAME}
+ * @description TODO
+ * @author ${USER}
+ * @create ${DATE} ${TIME}
+ */
+@SpringBootApplication
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
